4c80996fa5cdf07a0babf24b22bad0fa7811f3ea
Jeremy Hinegardner <jeremy@hinegardner.org>
2008-06-25 10:07:57 -0600

    update webby and current speaker infor
      * update to new version of webby
      * update the speakers.yaml file with July information and rebuild the site

--- Rakefile
+++ Rakefile
@@ -5,3 +5,12 @@ task :default => :build
 desc 'depoloy the site to the webserver'
 task :deploy => [:build, 'deploy:rsync']
 
+# Options passed to the 'tidy' program when the tidy filter is used
+SITE.tidy_options = '-indent -wrap 80'
+
+SITE.user = "blug"
+SITE.host = "newcommunity.tummy.com"
+SITE.remote_dir = '/home/httpd/lug.boulder.co.us/html'
+SITE.rsync_args = %w( -av )
+
+
--- tasks/setup.rb
+++ tasks/setup.rb
@@ -1,4 +1,3 @@
-# $Id$
 
 begin
   require 'webby'
@@ -9,26 +8,10 @@ end
 
 SITE = Webby.site
 
-# Webby defaults
-SITE.content_dir   = 'content'
-SITE.output_dir    = 'output'
-SITE.layout_dir    = 'layouts'
-SITE.template_dir  = 'templates'
-SITE.exclude       = %w[tmp$ bak$ ~$ CVS \.svn _darcs]
-  
-SITE.page_defaults = {
-  'layout' => 'default'
-}
-
-# Items used to deploy the webiste
-SITE.host       = 'blug@newcommunity.tummy.com'
-SITE.remote_dir = '/home/httpd/lug.boulder.co.us/html'
-SITE.rsync_args = %w( -av )
-
-# Options passed to the 'tidy' program when the tidy filter is used
-SITE.tidy_options = '-indent -wrap 80'
-
 # Load the other rake files in the tasks folder
-Dir.glob('tasks/*.rake').sort.each {|fn| import fn}
+Dir.glob(::File.join(%w[tasks *.rake])).sort.each {|fn| import fn}
+
+# Load all the ruby files in the lib folder
+Dir.glob(::File.join(%w[lib ** *.rb])).sort.each {|fn| require fn}
 
 # EOF
